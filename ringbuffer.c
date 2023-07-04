9bb0f347df1e3a0d8b99aae17e6395d1576ebb18
mucahit <mucahit.turkk@gmail.com>
2023-07-04 14:24:59 +0300

    deleted unnecessary command lines

--- ringbuffer.c
+++ ringbuffer.c
@@ -24,21 +24,21 @@ ringBufferStatus_e enqueue(ringBuffer_t *rb, uint32_t data)
     rb->tail = (rb->tail + 1) % BUFFER_SIZE;
     rb->count++;
 
-    return RINGBUFFER_SUCCESS_e; // Data successfully enqueued.
+    return RINGBUFFER_SUCCESS_e; 
 }
 
 ringBufferStatus_e dequeue(ringBuffer_t *rb, uint32_t *data) 
 {
     if (isEmpty(rb)) 
 	{
-        return RINGBUFFER_ERROR_EMPTY_e; // Returns an error when the ring buffer is empty.
+        return RINGBUFFER_ERROR_EMPTY_e;
     }
 
     *data = rb->buffer[rb->head];
     rb->head = (rb->head + 1) % BUFFER_SIZE;
     rb->count--;
 
-    return RINGBUFFER_SUCCESS_e; // Data successfully dequeued.
+    return RINGBUFFER_SUCCESS_e;
 }
 
 bool isFull(ringBuffer_t *rb) 
